ea32eb913d2df7364e9dc2551709fb52fad885bd
yardenavraham <yarden8bi@gmail.com>
2025-08-13 23:01:49 +0300

    roads to s3

--- scripts/ingestion/fetch_roads_to_s3.py
+++ scripts/ingestion/fetch_roads_to_s3.py
@@ -5,7 +5,12 @@ import os
 from datetime import date
 from shapely.geometry import LineString
 
-
+'''
+def fetch_roads_data(layer_code=577, 
+                     local_path="data/raw/roads_data.csv",
+                     s3_bucket=None,
+                     s3_prefix="raw/roads"):
+'''
 # API endpoint
 url = "https://gisn.tel-aviv.gov.il/GisOpenData/service.asmx/GetLayer"
 local_filename = "data/raw/roads_data.csv"
@@ -15,7 +20,7 @@ s3_key = f"raw/roads/{today}/roads_data.csv"
 
 # Parameters for the API request
 params = {
-    "layerCode": 577,       # Tel-O-Fun layer
+    "layerCode": 577,       # Roads layer
     "layerWhere": "",
     "xmin": "",
     "ymin": "",
@@ -34,26 +39,16 @@ data = response.json()
 features = data["features"]
 
 
-# יוצרים רשימה של dict שמשלבת attributes ו-geometry
 rows = []
 for feature in features:
-    row = feature['attributes'].copy()  # מאפייני השביל
-    row['geometry'] = feature['geometry']['paths'][0] # מוסיפים את הגיאומטריה כdict
+    row = feature['attributes'].copy()  
+    row['geometry'] = feature['geometry']['paths'][0] 
     rows.append(row)
 
 df = pd.DataFrame(rows)
 
 print(df.head())
 
-'''
-def paths_to_wkt(paths):
-    # paths זה רשימה של רשימות נקודות, למשל [[[x1,y1],[x2,y2],...]]
-    # נניח שמדובר בקו אחד (polyline)
-    line = LineString(paths[0])
-    return line.wkt
-df['geometry_wkt'] = df['geometry'].apply(lambda g: paths_to_wkt(g['paths']) if g else None)
-print(df[['oid_shvil', 'geometry_wkt']].head())
-'''
 
 os.makedirs(os.path.dirname(local_filename), exist_ok=True)
 df.to_csv(local_filename, index=False)
@@ -63,3 +58,5 @@ print(f"Local save: {local_filename}")
 s3 = boto3.client("s3")
 s3.upload_file(local_filename, s3_bucket, s3_key)
 print(f"Upload to s3://{s3_bucket}/{s3_key}")
+
+#return df
