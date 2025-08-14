82868bee7c3a0540ab08f02048a6bf6b9031f915
yardenavraham <yarden8bi@gmail.com>
2025-08-14 23:04:24 +0300

    roads transform

--- scripts/transform/roads_transform.py
+++ scripts/transform/roads_transform.py
@@ -3,42 +3,42 @@ from pyspark.sql import types as T
 from pyspark.sql import SparkSession
 from datetime import date
 import os
-
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
-
+#print(os.getcwd())
 access_key = os.getenv("ACCESS_KEY")
 secret_key = os.getenv("SECRET_KEY")   
+today = date.today().isoformat()
+local_parquet_path = "data/load/roads_data_p.parquet"
 
 
 
-print(os.getcwd())
+# ---- get parquet file with spark ----
 
 spark = SparkSession.builder \
     .master("local[*]") \
     .appName('roads_pipeline') \
     .getOrCreate()
 
-bucket_name = 'yarden-liron-pipeline'
-pipeline_name = 'roads'
-today = date.today().isoformat()
-
-local_parquet_path = "/home/developer/projects/lab/roads_data_p.parquet"
-
 df = spark.read.parquet(local_parquet_path)
 
-
+print(list(df.columns))
+# ---- change columns names ----
 df = df.withColumnRenamed("oid_shvil", "road_id") \
        .withColumnRenamed("shem_mikta", "road_name") \
        .withColumnRenamed("bitzua", "year_exec") \
        .withColumnRenamed("ms_orech", "length") \
        .withColumnRenamed("miflas", "surface_type") \
-              
+       .withColumnRenamed("Shape_Length", "shape_length") 
+print(list(df.columns))     
 
-
+#df.select("surface_type").distinct().show()   
+#df.select("geometry").show(5, truncate=False)  
+    
 df.show(5)
+
+# ---- spark to s3 ----
 
 bucket_name = "yarden-liron-processed-data"
 output_path = f"s3a://{bucket_name}/processed/roads/{today}"
@@ -51,6 +51,6 @@ df.write.mode("overwrite").parquet(output_path)
 
 print(f"DataFrame written to {output_path}")
 
-df.select("geometry").show(5, truncate=False)
+
 
 spark.stop()
